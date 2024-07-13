ee99664882b7c1f35dc2c87988486f5cae88486e
aftabansari9680 <aaftab9680@gmail.com>
2024-07-13 15:22:20 +0530

    fixed

--- app/page.js
+++ app/page.js
@@ -1,23 +1,12 @@
-"use client"
-import React, { useState } from 'react'
+import Counter from "@/components/counter"
 
-const page = () => {
-    const[number,setnumber]=useState(0)
-    function p() {
-        setnumber(number+11)
-    }
-    function d() {
-        setnumber(number-11)
-    }
+
+
+const Home = () => {
+
   return (
-    <div className=" flex flex-col justify-center items-center gap-4 h-screen w-full bg-yellow-700">
-        <div>{number}</div>
-        <div className="flex gap-5">
-        <div className="h-10 w-10 bg-emerald-800 " onClick={p}>+</div>
-        <div className="h-10 w-10 bg-red-800 " onClick={d}>-</div>
-        </div>
-    </div>
+<Counter/>
   )
 }
 
-export default page
+export default Home
---
+++ components/counter.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+
+const Counter = () => {
+  const [number, setnumber] = useState(0);
+  function p() {
+    setnumber(number + 11);
+  }
+  function d() {
+    setnumber(number - 11);
+  }
+  return (
+    <div className=" flex flex-col justify-center items-center gap-4 h-screen w-full bg-yellow-700">
+      <div>{number}</div>
+      <div className="flex gap-5">
+        <div className="h-10 w-10 bg-emerald-800 " onClick={p}>
+          +
+        </div>
+        <div className="h-10 w-10 bg-red-800 " onClick={d}>
+          -
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
